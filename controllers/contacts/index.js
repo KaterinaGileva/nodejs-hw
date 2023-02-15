1a687961f264857729845ca9b3c3e450d907e691
Katya <katyagilevaukr@gmail.com>
2023-02-15 12:32:42 +0200

    added controlles, middleware

---
+++ controllers/contacts/index.js
@@ -0,0 +1,13 @@
+const getAllContacts = require("./getAllContacts");
+const getById = require("./getById");
+const addContacts = require("./addContacts");
+const deleteContacts = require("./deleteContacts");
+const updateContacts = require("./updateContacts");
+
+module.exports = {
+    getAllContacts,
+    getById,
+    addContacts,
+    deleteContacts,
+    updateContacts
+}
