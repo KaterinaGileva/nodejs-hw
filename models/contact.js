6a204bee5cf89326c7335722d3c4e01f48fe811f
Katya <katyagilevaukr@gmail.com>
2023-02-24 11:13:00 +0200

    update

--- models/contact.js
+++ models/contact.js
@@ -1,15 +1,11 @@
 const {Schema, model} = require ("mongoose");
-//const Joi = require("joi");
+const Joi = require("joi");
 
-//const {handleSchemaValidationError} = require("../helpers");
+const {handleSchemaValidationError} = require("../helpers");
 
 const contactSchema = new Schema({
-  name: String,
-  email: String,
-  phone: String,
-  favorite: Boolean
-
-      /*  name: {
+ 
+       name: {
          type: String,
           required: [true, 'Set name for contact'],
         },
@@ -25,31 +21,31 @@ const contactSchema = new Schema({
          type: Boolean,
           default: false,
         },
-}, { versionKey: false, timestamps: true*/ }
+}, { versionKey: false, timestamps: true }
 );
 
-//contactSchema.post("save", handleSchemaValidationError);
+contactSchema.post("save", handleSchemaValidationError);
 
-//const addSchema = Joi.object({
- // name: Joi.string().required(),
- // email: Joi.string().required(),
-  //phone: Joi.string().required(),
-  //favorite: Joi.bool()
-  //});
+const addSchema = Joi.object({
+  name: Joi.string().required(),
+  email: Joi.string().required(),
+  phone: Joi.string().required(),
+  favorite: Joi.bool()
+  });
 
- //const updateFavoriteSchema =  Joi.object({
+ const updateFavoriteSchema =  Joi.object({
   
- // favorite: Joi.bool().required(),
- // })
+ favorite: Joi.bool().required(),
+  })
 
-//const schemas = {
-// addSchema,
- // updateFavoriteSchema,
-//}
+const schemas = {
+ addSchema,
+  updateFavoriteSchema,
+}
 
 const Contact = model("contact", contactSchema);
 
 module.exports = {
   Contact,
-  //schemas
+  schemas
 }
