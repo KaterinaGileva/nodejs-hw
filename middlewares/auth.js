5c79a4ab214ff645c67296f76be56df67f5b4942
Katya <katyagilevaukr@gmail.com>
2023-03-02 15:38:16 +0200

    update auth

--- middlewares/auth.js
+++ middlewares/auth.js
@@ -13,7 +13,7 @@ try{
         }
         const {id} = jwt.verify(token, SECRET_KEY);
         const user = await User.findById(id);
-        if(!user) {
+        if(!user || !user.token) {
              throw new Unauthorized("Not authorized");
     }
     req.user = user;
